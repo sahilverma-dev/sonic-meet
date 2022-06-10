6edf0aa5100f77cf0fe22ebca2c5e11a877cf2df
Sahil Verma <domainsahil007@gmail.com>
2022-06-10 13:02:23 +0530

    firebase config removed

--- client/src/firebase/config.js
+++ client/src/firebase/config.js
@@ -3,13 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDlFSyMqJ1IZGmrOzOvO4tpRO6hc2gyGPw",
-  authDomain: "testing-edf48.firebaseapp.com",
-  databaseURL: "https://testing-edf48-default-rtdb.firebaseio.com",
-  projectId: "testing-edf48",
-  storageBucket: "testing-edf48.appspot.com",
-  messagingSenderId: "322971047410",
-  appId: "1:322971047410:web:b14b7987e49e1011ef876b",
+  // your firebase config
 };
 
 const app = initializeApp(firebaseConfig);
